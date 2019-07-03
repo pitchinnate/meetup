0109ddc0136e9830c08bbc1ef632848ba763e1d4
Nate Denlinger <ndenlinger@smartlink.city>
2019-07-03 15:41:13 -0400

    new build

--- src/helper/http.js
+++ src/helper/http.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:8000/v1',
+  // baseURL: 'http://localhost:8000/v1',
+  baseURL: 'https://meetup-go-demo.herokuapp.com/',
   // headers: {
   //   Authorization: 'Bearer {token}',
   // },
